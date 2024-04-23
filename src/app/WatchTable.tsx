e5cfc2b11b2688916cb0fdd91ed0b44d48034bdf
Aleksey <svetlampy@mail.ru>
2024-04-23 20:27:01 +0500

    add data to WatchTable

--- src/app/WatchTable.tsx
+++ src/app/WatchTable.tsx
@@ -7,19 +7,21 @@ interface tasksProps {
   tasks: ITasks[];
 }
 
-const select = <Select defaultValue={["val_1"]}>
-<Select.Option value="val_1">Value 1</Select.Option>
-<Select.Option value="val_2">Value 2</Select.Option>
-<Select.Option value="val_3">Value 3</Select.Option>
-<Select.Option value="val_4">Value 4</Select.Option>
-</Select>
+const select = (
+  <Select defaultValue={["val_1"]}>
+    <Select.Option value="val_1">Value 1</Select.Option>
+    <Select.Option value="val_2">Value 2</Select.Option>
+    <Select.Option value="val_3">Value 3</Select.Option>
+    <Select.Option value="val_4">Value 4</Select.Option>
+  </Select>
+);
 
-const button = <Button view="outlined">Button</Button>
-const getRowId = 'id';
+const button = <Button view="outlined">Button</Button>;
+const getRowId = "id";
 
 const data = [
-  {id: 1, name: 'Hello', company: button, status: select },
-  {id: 2, name: 'World', company: button, status: select}
+  { id: 1, name: "Hello", company: button, status: select },
+  { id: 2, name: "World", company: button, status: select },
 ];
 
 // const columns = [{ id: "id" }, { id: "text" }, { id: "age" }];
@@ -31,13 +33,12 @@ const columns = [
 ];
 
 const MyTable: React.FC<tasksProps> = ({ tasks }) => {
-  
   const theme = "light";
   return (
     <ThemeProvider theme={theme}>
       <Table
         columns={columns}
-        data={data}
+        data={tasks}
         className="w-full flex justify-center"
       />
     </ThemeProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import { getAllTasks } from "@/api/api";
-
 import WatchTable from "./WatchTable";
+import { useEffect, useState } from "react";
+import { ITasks } from "@/types/tasks";
 
-export default async function Home() {
-  const tasks = await getAllTasks();
+export default function Home() {
+  const [tasks, setTasks] = useState<ITasks[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedTasks = await getAllTasks();
+      setTasks(fetchedTasks);
+    };
+
+    fetchData();
+  }, []);
   console.log(tasks);
-
   return (
     <div className="container">
       <WatchTable tasks={tasks} />
